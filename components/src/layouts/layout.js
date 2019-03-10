ee94ab5354b4d64901736188e7ecaba7566bde7a
Perry <Perrymoen@gmail.com>
2019-03-10 15:51:12 -0500

    Created Masthead component

--- components/src/layouts/layout.js
+++ components/src/layouts/layout.js
@@ -6,6 +6,7 @@ import "./layout.css"
 import {ThemeProvider} from 'styled-components'
 import {Gray as theme } from '../themes/Gray'
 import {Footer,Main} from '../components/Element'
+import {Masthead} from '../components/Masthead'
 
 
 const Layout = ({ children }) => (
@@ -22,7 +23,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Masthead siteTitle={data.site.siteMetadata.title} height= "75px" />
         <div
           style={{
             margin: `0 auto`,
